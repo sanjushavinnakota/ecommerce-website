157f69dce7dd385cd5f672de0deb5e3470b30e61
sanjushavinnakota <sanjushavinnakota@gmail.com>
2025-08-02 22:25:40 +0530

    changes pushed

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// require('dotenv').config();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const sellerRoutes = require('./routes/sellerRoutes');
-app.use('/api/sellers', sellerRoutes);
+// const sellerRoutes = require('./routes/sellerRoutes');
+// app.use('/api/sellers', sellerRoutes);
 
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+// const PORT = process.env.PORT || 5050;
+// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
